9b474660781c1c12ae868a79b6f0c9dd01cb78a9
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-11-04 23:59:46 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m6/ADS-2Week-1/Bag.java
+++ ADS-2-Assignments/m6/ADS-2Week-1/Bag.java
@@ -1,123 +1,79 @@
-/******************************************************************************
+/*************************************************************************
  *  Compilation:  javac Bag.java
  *  Execution:    java Bag < input.txt
- *  Dependencies: StdIn.java StdOut.java
  *
- *  A generic bag or multiset, implemented using a singly linked list.
+ *  A generic bag or multiset, implemented using a linked list.
  *
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Bag < tobe.txt
- *  size of bag = 14
- *  is
- *  -
- *  -
- *  -
- *  that
- *  -
- *  -
- *  be
- *  -
- *  to
- *  not
- *  or
- *  be
- *  to
- *
- ******************************************************************************/
-
+ *************************************************************************/
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- *  The {@code Bag} class represents a bag (or multiset) of 
+ *  The <tt>Bag</tt> class represents a bag (or multiset) of 
  *  generic items. It supports insertion and iterating over the 
  *  items in arbitrary order.
  *  <p>
- *  This implementation uses a singly linked list with a static nested class Node.
- *  See {@link LinkedBag} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
  *  take constant time. Iteration takes time proportional to the number of items.
  *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Item> the generic type of an item in this bag
  */
 public class Bag<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of bag
-    private int n;               // number of elements in bag
+    private int N;         // number of elements in bag
+    private Node first;    // beginning of bag
 
     // helper linked list class
-    private static class Node<Item> {
+    private class Node {
         private Item item;
-        private Node<Item> next;
+        private Node next;
     }
 
-    /**
-     * Initializes an empty bag.
+   /**
+     * Create an empty stack.
      */
     public Bag() {
         first = null;
-        n = 0;
+        N = 0;
     }
 
-    /**
-     * Returns true if this bag is empty.
-     *
-     * @return {@code true} if this bag is empty;
-     *         {@code false} otherwise
+   /**
+     * Is the BAG empty?
      */
     public boolean isEmpty() {
         return first == null;
     }
 
-    /**
-     * Returns the number of items in this bag.
-     *
-     * @return the number of items in this bag
+   /**
+     * Return the number of items in the bag.
      */
     public int size() {
-        return n;
+        return N;
     }
 
-    /**
-     * Adds the item to this bag.
-     *
-     * @param  item the item to add to this bag
+   /**
+     * Add the item to the bag.
      */
     public void add(Item item) {
-        Node<Item> oldfirst = first;
-        first = new Node<Item>();
+        Node oldfirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldfirst;
-        n++;
+        N++;
     }
 
 
-    /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary order.
-     *
-     * @return an iterator that iterates over the items in this bag in arbitrary order
+   /**
+     * Return an iterator that iterates over the items in the bag.
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator();  
     }
 
     // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
 
         public boolean hasNext()  { return current != null;                     }
         public void remove()      { throw new UnsupportedOperationException();  }
@@ -129,4 +85,5 @@ public class Bag<Item> implements Iterable<Item> {
             return item;
         }
     }
+
 }
