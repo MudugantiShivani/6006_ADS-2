ce1a88de04deb46fb8cfc15a22bcc7449e74ab9b
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-11-08 12:12:23 +0530

    "ads2_m10_a1_1: 0/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-Assignments/m10/Seam Carving/SeamCarver.java
@@ -1,26 +1,59 @@
-
+import java.awt.Color;
+import java.lang.Math;
 public class SeamCarver {
 	// create a seam carver object based on the given picture
+	Picture picture;
+	int width;
+	int height;
+	double energy[][];
 	public SeamCarver(Picture picture) {
+		this.picture = picture;
+		this.width = picture.width();
+		this.height = picture.height();
+		this.energy = new double[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height ; j++) {
+				if (i == 0 || j == 0) {
+					energy[i][j] = 1000.0;
+				} else {
+					energy[i][j] = squareroot(i, j);
+				}
+			}
+		}
 
+	}
+	public double squareroot(int i, int j) {
+		int xaxis = xaxis(i, j);
+		// int yaxis = yaxis(i, j);
+		double energy1 = Math.sqrt(xaxis);
+		return energy1;
+	}
+	public int xaxis(int i, int j) {
+		Color element1 = picture.get(i - 1, j);
+		Color element2 = picture.get(i + 1, j);
+		int r = element1.getRed() - element2.getRed();
+		int g = element1.getGreen() - element2.getGreen();
+		int b = element1.getBlue() - element2.getBlue();
+		int xaxis = (r * r) + (g * g) + (b * b);
+		return xaxis;
 	}
 	// current picture
 	public Picture picture() {
-		return null;
+		return picture;
 	}
 	// width of current picture
 	public int width() {
-		return 0;
+		return width;
 	}
 
 	// height of current picture
 	public int height() {
-		return 0;
+		return height;
 	}
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		return energy[x][y];
 	}
 
 	// sequence of indices for horizontal seam
