ef907df7a988b679e353eb073857c027fb0dcebd
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-11-10 12:30:26 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-Assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,36 +1,49 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeighted edgeGraph = new EdgeWeighted(vertices);
-		for (int i = 0; i < edges; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			edgeGraph.addEdge(new Edge(Integer.parseInt(tokens[0]),
+    }
+    /**
+     * {main class for solution}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeighted edgeGraph = new EdgeWeighted(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            edgeGraph.addEdge(new Edge(Integer.parseInt(tokens[0]),
                                     Integer.parseInt(tokens[1]),
                              Double.parseDouble(tokens[2])));
-			// edgeGraph.addEdge(newedge);
-		}
+            // edgeGraph.addEdge(newedge);
+        }
 
-		String caseToGo = scan.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-			//Print the Graph Object.
-			System.out.println(edgeGraph.toString());
-			break;
+        String caseToGo = scan.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+            //Print the Graph Object.
+            System.out.println(edgeGraph.toString());
+            break;
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			
-			String[] item = scan.nextLine().split(" ");
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            
+            String[] item = scan.nextLine().split(" ");
             Dijk dijk = new Dijk(edgeGraph, Integer.parseInt(item[0]));
             double destination = dijk.distanceTo(Integer.parseInt(
                                             item[1]));
@@ -41,18 +54,19 @@ public class Solution {
             }
             break;
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			System.out.println("No Path Found.");
-			break;
+        case "ViaPaths":
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is the via is the one where
+            // path should pass throuh.
+            // third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            System.out.println("No Path Found.");
+            break;
 
-		default:
-			break;
-		}
+        default:
+            break;
+        }
 
-	}
+    }
 }
