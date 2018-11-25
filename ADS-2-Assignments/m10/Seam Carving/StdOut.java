2d0d5b610501715f7b43d63f423c218e9acd3930
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-11-25 23:52:04 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m10/Seam Carving/StdOut.java
+++ ADS-2-Assignments/m10/Seam Carving/StdOut.java
@@ -13,15 +13,18 @@ import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 
 /**
- *  This class provides methods for printing strings and numbers to standard output.
+ *  This class provides methods for printing strings and
+ *  numbers to standard output.
  *  <p>
  *  <b>Getting started.</b>
  *  To use this class, you must have {@code StdOut.class} in your
  *  Java classpath. If you used our autoinstaller, you should be all set.
  *  Otherwise, either download
- *  <a href = "https://introcs.cs.princeton.edu/java/code/stdlib.jar">stdlib.jar</a>
+ *  <a href = "https://introcs.cs.princeton.edu/java/code/stdlib.jar">
+ *  stdlib.jar</a>
  *  and add to your Java classpath or download
- *  <a href = "https://introcs.cs.princeton.edu/java/stdlib/StdOut.java">StdOut.java</a>
+ *  <a href = "https://introcs.cs.princeton.edu/java/stdlib/StdOut.java">
+ *  StdOut.java</a>
  *  and put a copy in your working directory.
  *  <p>
  *  Here is an example program that uses {@code StdOut}:
@@ -61,19 +64,26 @@ import java.util.Locale;
  */
 public final class StdOut {
 
-    // force Unicode UTF-8 encoding; otherwise it's system dependent
+    /**
+     * {string}.
+     */
     private static final String CHARSET_NAME = "UTF-8";
 
-    // assume language = English, country = US for consistency with StdIn
+    /**
+     * { var_description }.
+     */
     private static final Locale LOCALE = Locale.US;
 
-    // send output here
+    /**
+     * { var_description }.
+     */
     private static PrintWriter out;
 
     // this is called before invoking any methods
     static {
         try {
-            out = new PrintWriter(new OutputStreamWriter(System.out, CHARSET_NAME), true);
+            out = new PrintWriter(new OutputStreamWriter(
+                System.out, CHARSET_NAME), true);
         }
         catch (UnsupportedEncodingException e) {
             System.out.println(e);
@@ -81,7 +91,8 @@ public final class StdOut {
     }
 
     // don't instantiate
-    private StdOut() { }
+    private StdOut() {
+    }
 
    /**
      * Closes standard output.
@@ -89,6 +100,9 @@ public final class StdOut {
      *             subsequent calls to StdOut.println() or System.out.println()
      *             will no longer produce output on standard output.
      */
+   /**
+    * { function_description }.
+    */
     @Deprecated
     public static void close() {
         out.close();
@@ -106,7 +120,7 @@ public final class StdOut {
      *complexity is O(1).
      * @param x the object to print
      */
-    public static void println(Object x) {
+    public static void println(final Object x) {
         out.println(x);
     }
 
@@ -115,7 +129,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the boolean to print
      */
-    public static void println(boolean x) {
+    public static void println(final boolean x) {
         out.println(x);
     }
 
@@ -124,7 +138,7 @@ public final class StdOut {
      *complexity is O(1).
      * @param x the character to print
      */
-    public static void println(char x) {
+    public static void println(final char x) {
         out.println(x);
     }
 
@@ -133,7 +147,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the double to print
      */
-    public static void println(double x) {
+    public static void println(final double x) {
         out.println(x);
     }
 
@@ -142,7 +156,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the integer to print
      */
-    public static void println(float x) {
+    public static void println(final float x) {
         out.println(x);
     }
 
@@ -151,7 +165,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the integer to print
      */
-    public static void println(int x) {
+    public static void println(final int x) {
         out.println(x);
     }
 
@@ -160,7 +174,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the long to print
      */
-    public static void println(long x) {
+    public static void println(final long x) {
         out.println(x);
     }
 
@@ -169,7 +183,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the short to print
      */
-    public static void println(short x) {
+    public static void println(final short x) {
         out.println(x);
     }
 
@@ -180,7 +194,7 @@ public final class StdOut {
      *
      * @param x the byte to print
      */
-    public static void println(byte x) {
+    public static void println(final byte x) {
         out.println(x);
     }
 
@@ -197,7 +211,7 @@ public final class StdOut {
      *  complexity is O(1).
      * @param x the object to print
      */
-    public static void print(Object x) {
+    public static void print(final Object x) {
         out.print(x);
         out.flush();
     }
@@ -207,7 +221,7 @@ public final class StdOut {
      *  complexity is O(1).
      * @param x the boolean to print
      */
-    public static void print(boolean x) {
+    public static void print(final boolean x) {
         out.print(x);
         out.flush();
     }
@@ -217,7 +231,7 @@ public final class StdOut {
      *  complexity is O(1).
      * @param x the character to print
      */
-    public static void print(char x) {
+    public static void print(final char x) {
         out.print(x);
         out.flush();
     }
@@ -227,7 +241,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the double to print
      */
-    public static void print(double x) {
+    public static void print( final double x) {
         out.print(x);
         out.flush();
     }
@@ -237,7 +251,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the float to print
      */
-    public static void print(float x) {
+    public static void print(final float x) {
         out.print(x);
         out.flush();
     }
@@ -247,7 +261,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the integer to print
      */
-    public static void print(int x) {
+    public static void print(final int x) {
         out.print(x);
         out.flush();
     }
@@ -257,7 +271,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the long integer to print
      */
-    public static void print(long x) {
+    public static void print(final long x) {
         out.print(x);
         out.flush();
     }
@@ -267,7 +281,7 @@ public final class StdOut {
      * complexity is O(1).
      * @param x the short integer to print
      */
-    public static void print(short x) {
+    public static void print(final short x) {
         out.print(x);
         out.flush();
     }
@@ -277,7 +291,7 @@ public final class StdOut {
      *complexity is O(1).
      * @param x the byte to print
      */
-    public static void print(byte x) {
+    public static void print(final byte x) {
         out.print(x);
         out.flush();
     }
@@ -287,10 +301,11 @@ public final class StdOut {
      * string and arguments, and then flushes standard output.
      *
      *
-     * @param format the <a href = "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax">format string</a>
+     * @param format the <a href = "http://docs.oracle.com/javase/7/docs/api/
+     * java/util/Formatter.html#syntax">format string</a>
      * @param args   the arguments accompanying the format string
      */
-    public static void printf(String format, Object... args) {
+    public static void printf(final String format, final Object... args) {
         out.printf(LOCALE, format, args);
         out.flush();
     }
@@ -300,10 +315,12 @@ public final class StdOut {
      * the specified format string and arguments; then flushes standard output.
      *
      * @param locale the locale
-     * @param format the <a href = "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax">format string</a>
+     * @param format the <a href = "http://docs.oracle.com/javase/7/docs/api/
+     * java/util/Formatter.html#syntax">format string</a>
      * @param args   the arguments accompanying the format string
      */
-    public static void printf(Locale locale, String format, Object... args) {
+    public static void printf(final Locale locale, final String format,
+        final Object... args) {
         out.printf(locale, format, args);
         out.flush();
     }
